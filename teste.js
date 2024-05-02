b30dd72b711cad08f03cc25360cd61ccc8112394
Ryan122dias <162304403+Ryan122dias@users.noreply.github.com>
2024-05-02 08:49:12 -0400

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,71 @@
+/* comentário de multiplas linhas */
+
+/*
+  variáveis
+  espaço na mermoria reservado,
+  o js não é estipado
+
+
+
+*/
+// comentário de uma linha só
+ //var nome1 ="Ivanildo"
+//console.log(" o conteúdo da variável é:" + nome1)
+
+
+ //let nome2 = "Ivanildo amarim paivaa"
+// console.log("o conteúdo da variável é:" + nome2)
+
+// const nome3 = "Ivanildo"
+// console.log("o conteúdo da variável é:" + nome3)
+
+//alert
+//alert("digite seu nome")
+// //promp para entrada de dados
+//window.prompt("nome")
+
+//arrys
+//lat a =["jose","maria","joão",]
+//let a = {
+    // nome: "Ivanildo",
+    //sobrenome:"amorim",]
+    //idade:28
+//};
+
+//console.log(" o nome do aluno é" +' ${a.nome} 
+// sobre nome é ${a.sobrenome} e sua idade é ${a.idade}
+//');
+
+
+// let a = 2;
+//let a = 3;// concatenação estou juntando o valor da variável + o texto
+
+//console.log(a)//operações matemática
+
+//funções
+
+// aqui estou criando a função
+
+/*function somanumero(a,b){
+        var a;
+        var b;
+
+}
+// aqui estou chamando a função
+somanumero(5+6)
+//aqui estou exibindo o valor da função
+console.log(5+6)
+*/
+//let nome = window.prompt("digite seu nome")
+//let senha = window.prompt("digite sua senha")
+
+//console.log("seja bem" + "-" + "vindo sr" + nome)
+//console.log("sua" + "-" + "senha é:" + senha)
+function login(){
+  let usuario = document.getElementById("user").Value;
+
+  let senha = document.getElementById("user").Value;
+  
+}
+
+
